3dd492afd027136f9a2a8e227408c64eb39b9faa
sl4049 <sl4049@tc.columbia.edu>
2017-10-23 04:08:18 -0400

    added dropdown menu

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,26 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+function howtodp() {
+    document.getElementById("howtodp").classList.toggle("show");
+}
+function aboutdp() {
+    document.getElementById("aboutdp").classList.toggle("show");
+}
+
+function headlinesdp() {
+    document.getElementById("headlinesdp").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropa')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
