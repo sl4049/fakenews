2642f624293392c7f22bfcd0e29a827f402238bb
Andrea Mozo <andreamozolopez@gmail.com>
2017-12-17 23:17:36 -0500

    Drag and Drop
    The drag and drop elements uploaded within this branch were found using this youtube video: https://www.youtube.com/watch?v=U2tEzqk4F9g It is very basic. It seems as though I need to work with npm. I will take a deeper look tomorrow.

---
+++ fakenews/public/js/myjs.js
@@ -0,0 +1,47 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+function saveUrl() {
+  var url = document.getElementById("urlInput").value;
+    sessionStorage.setItem("inputurl", url);
+}
+
+function loadArticle () {
+  var articleEl = document.getElementById('frame');
+  articleEl.src = sessionStorage.getItem("inputurl");
+}
+
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function dragStart(ev) {
+  id=ev.target.id;
+}
+
+function drop (ev) {
+  ev.target.append(document.getElementById(id));
+}
+
+//"http://worldnewsdailyreport.com/woman-arrested-for-training-squirrels-to-attack-her-ex-boyfriend/";
